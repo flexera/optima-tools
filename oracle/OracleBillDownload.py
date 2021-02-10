b325fbca16f72b05c41a624881d0ba617ace8506
Richard Shade <rshade@flexera.com>
2021-02-10 15:52:06 -0600

    Updating per peer review

--- oracle/OracleBillDownload.py
+++ oracle/OracleBillDownload.py
@@ -1,19 +1,21 @@
+'''
+This script downloads all of the cost, usage, (or both) reports for a tenancy (specified in the config file).
+
+Pre-requisites: Create an IAM policy to endorse users in your tenancy to read cost reports from the OCI tenancy.
+
+Example policy:
+define tenancy reporting as ocid1.tenancy.oc1..aaaaaaaaned4fkpkisbwjlr56u7cj63lf3wffbilvqknstgtvzub7vhqkggq
+endorse group group_name to read objects in tenancy reporting
+
+Note - The only value you need to change is the group name. Do not change the OCID in the first statement.
+'''
+
 import oci
 import os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import pytz
 import json
-
-# This script downloads all of the cost, usage, (or both) reports for a tenancy (specified in the config file).
-#
-# Pre-requisites: Create an IAM policy to endorse users in your tenancy to read cost reports from the OCI tenancy.
-#
-# Example policy:
-# define tenancy reporting as ocid1.tenancy.oc1..aaaaaaaaned4fkpkisbwjlr56u7cj63lf3wffbilvqknstgtvzub7vhqkggq
-# endorse group group_name to read objects in tenancy reporting
-#
-# Note - The only value you need to change is the group name. Do not change the OCID in the first statement.
 
 reporting_namespace = 'bling'
 
@@ -23,15 +25,14 @@ prefix_file = ""                     #  For cost and usage files
 # prefix_file = "reports/usage-csv"  #  For usage
 
 # Update these values
-destintation_path = 'CBI'
+destination_path = 'CBI'
 
 # Download all file or just past three months.
-env_download_all = os.environ.get("DOWNLOAD_ALL")
-download_all_files = bool(env_download_all)
+download_all_files = os.environ.get("DOWNLOAD_ALL", False)
 
 # Make a directory to receive reports
-if not os.path.exists(destintation_path):
-    os.mkdir(destintation_path)
+if not os.path.exists(destination_path):
+    os.mkdir(destination_path)
 
 # Get the list of reports
 config = oci.config.from_file(oci.config.DEFAULT_LOCATION, oci.config.DEFAULT_PROFILE)
@@ -59,12 +60,12 @@ for o in report_bucket_objects.data.objects:
   if download:
     print('Found file ' + o.name)
     folder_time = o.time_modified.strftime("%Y-%m")
-    download_folder = os.path.join(destintation_path,folder_time)
+    download_folder = os.path.join(destination_path,folder_time)
     # Make a directory to receive reports
     if not os.path.exists(download_folder):
       os.mkdir(download_folder)
 
-    filename = o.name.rsplit('/', 1)[-1]
+    filename = os.path.basename(o.name)
     written_file_name = os.path.join(download_folder, filename)
     downloaded_files.append(written_file_name)
 
