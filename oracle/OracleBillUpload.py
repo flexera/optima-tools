2a1350795c39e3d704d6a2ae2a579e0dc21f6d21
Richard Shade <rshade@flexera.com>
2021-02-25 10:35:56 -0600

    moving loop up a level

--- oracle/OracleBillUpload.py
+++ oracle/OracleBillUpload.py
@@ -11,7 +11,6 @@ logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', stream=sys.s
 refresh_token = os.environ.get('REFRESH_TOKEN')
 org_id = os.environ.get('ORG_ID')
 bill_connect_id = os.environ.get("BILL_CONNECT_ID")
-period = os.environ.get("PERIOD")
 shard = os.environ.get("SHARD")
 
 if not shard == "3" and not shard == "4":
@@ -33,13 +32,6 @@ access_token = r.json()["access_token"]
 auth_headers = {"Authorization": "Bearer " + access_token}
 kwargs = {"headers": auth_headers, "allow_redirects": False}
 
-logging.info("1. Creating Bill Upload...")
-bill_upload = {"billConnectId": bill_connect_id, "billingPeriod": period}
-r = requests.post(bill_upload_url, json.dumps(bill_upload), **kwargs)
-logging.info("Response: {}\n{}".format(r.status_code, json.dumps(r.json(), indent=4)))
-r.raise_for_status()
-bill_upload_id = r.json()["id"]
-
 # uploading files to endpoint
 dir_path =  os.path.dirname(os.path.realpath(__file__))
 json_file = os.path.join(dir_path, 'files.json')
@@ -48,17 +40,25 @@ with open(json_file) as f:
   data = json.load(f)
 
 for filename in data:
+  period = filename.split('\\')[1])
+  logging.info("1. Creating Bill Upload for Period:" + period)
+  bill_upload = {"billConnectId": bill_connect_id, "billingPeriod": period}
+  r = requests.post(bill_upload_url, json.dumps(bill_upload), **kwargs)
+  logging.info("Response: {}\n{}".format(r.status_code, json.dumps(r.json(), indent=4)))
+  r.raise_for_status()
+  bill_upload_id = r.json()["id"]
+
   base_name = os.path.basename(filename)
   logging.info("2. Upload file {} to Bill Upload {}...".format(base_name, bill_upload_id))
   upload_file_url = "{}/{}/files/{}".format(bill_upload_url, bill_upload_id, base_name)
   r = requests.post(upload_file_url, data=open(filename, 'rb').read(), **kwargs)
   logging.info("Response: {}\n{}".format(r.status_code, json.dumps(r.json(), indent=4)))
 
-logging.info("3. Committing the Bill Upload {}...".format(bill_upload_id))
-operations_url = "{}/{}/operations".format(bill_upload_url, bill_upload_id)
-r = requests.post(operations_url, '{"operation":"commit"}', **kwargs)
-logging.info("Response: {}\n{}".format(r.status_code, json.dumps(r.json(), indent=4)))
-r.raise_for_status()
+  logging.info("3. Committing the Bill Upload {}...".format(bill_upload_id))
+  operations_url = "{}/{}/operations".format(bill_upload_url, bill_upload_id)
+  r = requests.post(operations_url, '{"operation":"commit"}', **kwargs)
+  logging.info("Response: {}\n{}".format(r.status_code, json.dumps(r.json(), indent=4)))
+  r.raise_for_status()
 
 # ===== That's all, folks! =====
 sys.exit(0)
