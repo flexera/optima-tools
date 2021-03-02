83a21cf473c13470d09f2e667ebfbec865c79a6a
Richard Shade <rshade@flexera.com>
2021-03-01 21:05:58 -0600

    adding terraform items

---
+++ common-bill-ingestion/oracle/tf/vars.tf
@@ -0,0 +1,5 @@
+variable compartment_ocid { }
+variable tenancy_ocid { }
+variable region { default = "us-ashburn-1" }
+variable tag_created_by { default = "oracleidentitycloudservice/rshade@flexerasoftware.com"}
+variable tag_created_on { default = timestamp() }
